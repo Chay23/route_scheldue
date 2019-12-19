c0520a0ecaac55eb28b64247995c1671e3a9decd
BogVin <34000503+BogVin@users.noreply.github.com>
2019-12-19 03:02:34 +0200

    Create Towns.sql

---
+++ Towns.sql
@@ -0,0 +1,3 @@
+INSERT INTO "Towns" (town_id, town_name)  VALUES( 1, 'Lviv');
+INSERT INTO "Towns" (town_id, town_name)  VALUES( 2, 'Ivano_Frankivsk');
+INSERT INTO "Towns" (town_id, town_name)  VALUES( 3, 'Kyiv');
