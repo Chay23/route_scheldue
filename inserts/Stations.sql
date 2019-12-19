6141edfbf0cae09df4b4ff00c71020b2dc5cfdb6
BogVin <34000503+BogVin@users.noreply.github.com>
2019-12-19 03:18:50 +0200

    Create Stations.sql

---
+++ inserts/Stations.sql
@@ -0,0 +1,17 @@
+INSERT INTO "Stations" (town , station_id, station_name) VALUES(1, 11, 'Tetralna');
+INSERT INTO "Stations" (town , station_id, station_name) VALUES(1, 12, 'Petlury');
+INSERT INTO "Stations" (town , station_id, station_name) VALUES(1, 13, 'Vokzal');
+INSERT INTO "Stations" (town , station_id, station_name) VALUES(1, 14, 'Kolumba');
+
+INSERT INTO "Stations" (town , station_id, station_name) VALUES(2, 21, 'Chornovola');
+INSERT INTO "Stations" (town , station_id, station_name) VALUES(2, 22, 'Dovchenka');
+INSERT INTO "Stations" (town , station_id, station_name) VALUES(2, 23, 'Mazepy');
+INSERT INTO "Stations" (town , station_id, station_name) VALUES(2, 24, 'Sacharova');
+INSERT INTO "Stations" (town , station_id, station_name) VALUES(2, 25, 'Pochta');
+INSERT INTO "Stations" (town , station_id, station_name) VALUES(2, 26, 'Bandery');
+
+
+INSERT INTO "Stations" (town , station_id, station_name) VALUES(3, 31, 'Platona');
+INSERT INTO "Stations" (town , station_id, station_name) VALUES(3, 32, 'Dnipro');
+INSERT INTO "Stations" (town , station_id, station_name) VALUES(3, 33, 'Dambova');
+INSERT INTO "Stations" (town , station_id, station_name) VALUES(3, 34, 'Sadova');
